cadd9e7878443a37b7fc3b66f8529f782323a6a3
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-18 17:13:25 +0000

    add auth routes

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+});
+
+Route::group([
+    'middleware' => ['api', 'auth:api'],
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::get('me', [AuthController::class, 'userProfile']);
+});
