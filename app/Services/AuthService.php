28e71c1e554f64b6b6470d51396e6c27ff021b2d
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-18 16:49:28 +0000

    implement AuthService [register | login | refresh | logout | getProfile ]

---
+++ app/Services/AuthService.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\UserRepositoryInterface;
+use Illuminate\Support\Facades\Hash;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
+class AuthService
+{
+    protected $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function register(array $data)
+    {
+        $data['password'] = Hash::make($data['password']);
+        $user = $this->userRepository->create($data);
+
+        if (isset($data['interests']) && $user->isStudent()) {
+            $user->interests()->sync($data['interests']);
+        }
+
+        return $user;
+    }
+
+    public function login(array $credentials)
+    {
+        if (!$token = auth('api')->attempt($credentials)) {
+            return null;
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return [
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ];
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth('api')->refresh());
+    }
+
+    public function logout()
+    {
+        auth('api')->logout();
+    }
+
+    public function getProfile()
+    {
+        $user = auth('api')->user();
+        return $user ? $user->load('interests') : null;
+    }
+}
