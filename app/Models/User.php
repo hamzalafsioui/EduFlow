6e0e25f8e5675aa06f398832d80be71ae0e9d23c
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-18 17:12:35 +0000

    implements JWTSubject |  getJWTIdentifier to return user ID stored inside the token & getJWTCustomClaims to add extra data inside jwt payload

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable 
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -64,5 +64,25 @@ class User extends Authenticatable
         return $this->belongsToMany(Category::class, 'user_interests');
     }
 
-  
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+            'role' => $this->role,
+        ];
+    }
 }
