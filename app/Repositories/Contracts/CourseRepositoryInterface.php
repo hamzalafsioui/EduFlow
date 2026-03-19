3f670098ae1ed883237c99fb27655e99a8384bf3
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-19 15:38:42 +0000

    add getRecommended

--- app/Repositories/Contracts/CourseRepositoryInterface.php
+++ app/Repositories/Contracts/CourseRepositoryInterface.php
@@ -11,4 +11,5 @@ interface CourseRepositoryInterface
     public function create(array $data);
     public function update(int $id, array $data);
     public function delete(int $id);
+    public function getRecommended(array $categoryIds);
 }
