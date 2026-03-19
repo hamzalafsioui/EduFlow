61f85cebce5b28ab7a6a854b91304f14e97409e7
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-19 00:01:24 +0000

    Model Course

---
+++ app/Models/Course.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Category;
+
+class Course extends Model
+{
+    /** @use HasFactory<\Database\Factories\CourseFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'teacher_id',
+        'category_id',
+        'title',
+        'description',
+        'price',
+    ];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
