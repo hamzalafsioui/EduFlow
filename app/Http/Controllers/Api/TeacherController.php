593e3088813d6cbec0e8514e243f9cbc5668c04c
hamzalafsioui <hamzalafsioui@gmail.com>
2026-03-21 16:23:06 +0000

    add courseGroups

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Course;
 use App\Services\EnrollmentService;
+use App\Services\GroupService;
 use Illuminate\Support\Facades\Auth;
+use Exception;
 
 class TeacherController extends Controller
 {
     protected $enrollmentService;
+    protected $groupService;
 
-    public function __construct(EnrollmentService $enrollmentService)
+    public function __construct(EnrollmentService $enrollmentService, GroupService $groupService)
     {
         $this->enrollmentService = $enrollmentService;
+        $this->groupService = $groupService;
     }
 
     public function courseStudents(string $courseId)
@@ -23,7 +27,7 @@ class TeacherController extends Controller
             $course = Course::findOrFail((int)$courseId);
             $students = $this->enrollmentService->getCourseStudents($user, $course);
             return response()->json($students);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return response()->json(['error' => $e->getMessage()], 403);
         }
     }
@@ -38,4 +42,18 @@ class TeacherController extends Controller
             return response()->json(['error' => $e->getMessage()], 403);
         }
     }
+
+    public function courseGroups(string $courseId)
+    {
+        try {
+            $user = Auth::user();
+            $course = Course::findOrFail((int)$courseId);
+            $groups = $this->groupService->getCourseGroups($user, $course);
+            return response()->json($groups);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 403);
+        }
+    }
+
+   
 }
